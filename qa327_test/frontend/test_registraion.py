7d084d5a8ae1926637265fc7eab3f1d1be18d11b
Ekimerton <ekim0252@gmail.com>
2020-11-18 20:00:18 -0500

    Tweaked preexisting tests so they pass 🎉

--- qa327_test/frontend/test_registraion.py
+++ qa327_test/frontend/test_registraion.py
@@ -23,9 +23,9 @@ Annotate @patch before unit tests can mock backend methods (for that testing fun
 
 # Moch a sample user
 test_user = User(
-    email='test_frontend@test.com',
-    name='test_frontend',
-    password=generate_password_hash('test_frontend')
+    email='tester0@gmail.com',
+    name='Tester Zero',
+    password=generate_password_hash('Password123')
 )
 
 # Moch some sample tickets
@@ -46,26 +46,25 @@ class FrontEndHomePageTest(BaseCase):
         # open login page
         self.open(base_url + '/login')
         # fill email and password
-        self.type("#email", "test_frontend@test.com")
-        self.type("#password", "test_frontend")
+        self.type("#email", "tester0@gmail.com")
+        self.type("#password", "Password123")
         # click enter button
         self.click('input[type="submit"]')
-        
+
         # after clicking on the browser (the line above)
-        # the front-end code is activated 
+        # the front-end code is activated
         # and tries to call get_user function.
         # The get_user function is supposed to read data from database
         # and return the value. However, here we only want to test the
-        # front-end, without running the backend logics. 
-        # so we patch the backend to return a specific user instance, 
+        # front-end, without running the backend logics.
+        # so we patch the backend to return a specific user instance,
         # rather than running that program. (see @ annotations above)
-        
-        
+
         # open home page
         self.open(base_url)
         # test if the page loads correctly
         self.assert_element("#welcome-header")
-        self.assert_text("Welcome test_frontend", "#welcome-header")
+        self.assert_text("Hi Tester Zero !", "#welcome-header")
         self.assert_element("#tickets div h4")
         self.assert_text("t1 100", "#tickets div h4")
 
@@ -76,10 +75,10 @@ class FrontEndHomePageTest(BaseCase):
         # open login page
         self.open(base_url + '/login')
         # fill wrong email and password
-        self.type("#email", "test_frontend@test.com")
-        self.type("#password", "wrong_password")
+        self.type("#email", "tester0@gmail.com")
+        self.type("#password", "Password124")
         # click enter button
         self.click('input[type="submit"]')
         # make sure it shows proper error message
         self.assert_element("#message")
-        self.assert_text("login failed", "#message")
+        self.assert_text("email/password combination incorrect", "#message")
