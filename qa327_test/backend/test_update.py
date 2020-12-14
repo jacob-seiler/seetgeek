74e5a514bbc7a620c28303bcc1b0dd2615dcc0af
Jacob Seiler <seiler.jacob1@gmail.com>
2020-12-14 17:05:27 -0500

    Added R5 test case IDs

--- qa327_test/backend/test_update.py
+++ qa327_test/backend/test_update.py
@@ -45,47 +45,55 @@ class BackEndUpdateTest(BaseCase):
     def test_ticket_is_valid(self, *_):
         """
         The name of the ticket has to be alphanumeric-only - positive
+        Test case IDs: R5.1.1, R5.2.1, R5.3.1, R5.4.1, R5.5.1
         """
         return validate_ticket(test_ticket.name, test_ticket.quantity, test_ticket.price, test_ticket.expiration_date) == False
 
     def test_ticket_is_not_alphanumeric(self, *_):
         """
         The name of the ticket has to be alphanumeric-only - negative
+        Test case ID: R5.1.2
         """
         return validate_ticket('hello $$$', test_ticket.quantity, test_ticket.price, test_ticket.expiration_date) == "Name must have alphanumeric characters only."
 
     def test_ticket_name_is_too_long(self, *_):
         """
         The name of the ticket is no longer than 60 characters - negative
+        Test case ID: R5.2.2
         """
         return validate_ticket('thisis61characterslongaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', test_ticket.quantity, test_ticket.price, test_ticket.expiration_date) == "Name must be less than 60 characters."
 
     def test_ticket_quantity_is_not_valid(self, *_):
         """
         The quantity of the tickets has to be more than 0, and less than or equal to 100. - negative
+        Test case ID: R5.3.2
         """
         return validate_ticket(test_ticket.name, '0', test_ticket.price, test_ticket.expiration_date) == "Quantity must be between 1 and 100."
 
     def test_ticket_price_is_not_valid(self, *_):
         """
         Price has to be of range [10, 100] - negative
+        Test case ID: R5.4.2
         """
         return validate_ticket(test_ticket.name, test_ticket.quantity, '0', test_ticket.expiration_date) == "Price must be between 10 and 100 inclusive."
 
     def test_ticket_date_is_not_valid(self, *_):
         """
         Date must be given in the format YYYYMMDD - negative
+        Test case ID: R5.5.2
         """
         return validate_ticket(test_ticket.name, test_ticket.quantity, test_ticket.price, '123456789') == "Date must be in the format YYYYMMDD."
 
     def test_ticket_does_exist(self, *_):
         """
         The ticket of the given name must exist - positive
+        Test case ID: R5.6.1
         """
         return ticket_exists(test_ticket.name)
 
     def test_ticket_does_not_exist(self, *_):
         """
         The ticket of the given name must exist - negative
+        Test case ID: R5.6.2
         """
         return not ticket_exists('fakeTicketName')
